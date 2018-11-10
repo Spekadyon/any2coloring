6499303ff24d24c6b2965632545b4bb09c0d504d
Geoffrey Brun <geoffrey+git@spekadyon.org>
2018-11-10 11:59:33 +0100

    Add display palette tool

---
+++ display_palette.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2017 - Geoffrey Brun <geoffrey@spekadyon.org>
+ *
+ * This file is part of any2coloring.
+ *
+ * any2coloring is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * any2coloring is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * any2coloring. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include <cstdint>
+#include <cstdlib>
+
+#include <CImg.h>
+
+#include "any2col.hpp"
+
+using namespace std;
+using namespace cimg_library;
+
+int main(int argc, char *argv[])
+{
+	vector<struct color> palette;
+	CImg<float> cimg_palette;
+
+	if (argc != 2) {
+		cerr << "Usage:" << endl;
+		cerr << "\t" << argv[0] << " palette.csv" << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	read_palette(argv[1], palette);
+	palette2CImg(palette, cimg_palette);
+
+	cimg_palette.display();
+
+
+	return EXIT_SUCCESS;
+}
+
+
