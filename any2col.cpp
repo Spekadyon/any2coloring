1c149833bdf9232e716401fb509f87a175cb51e9
Geoffrey Brun <geoffrey+git@spekadyon.org>
2022-04-03 15:47:30 +0200

    Command line parsing: replace getopt by QCommandLineParser/QCommandLineOption

--- any2col.cpp
+++ any2col.cpp
@@ -17,219 +17,341 @@
  * any2coloring. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <QGuiApplication>
+#include <QCoreApplication>
+#include <QCommandLineParser>
+#include <QCommandLineOption>
 #include <QTimer>
 
-#include <iostream>
-#include <fstream>
-
-#include <cstdlib>
+#include <QDebug>
 
 #include "any2col.hpp"
 
-using namespace std;
-using namespace cimg_library;
-
-// getopt()
-#include <unistd.h>
-
-void help_exit(char *str)
+void printMissingOption(char const *str)
 {
-	fprintf(stdout, "Usage:\n");
-	fprintf(stdout, "\t%s [options] -p palette.csv -i <input_image> -o <coloring.svg>\n\n", basename(str));
-	fprintf(stdout, "Options:\n");
-	fprintf(stdout, "\t-h                  print help and exit\n");
-	fprintf(stdout, "\t-p <palette.csv>    color palette (mandatory)\n");
-	fprintf(stdout, "\t-i <input.png>      input picture (mandatory)\n");
-//	fprintf(stdout, "\t-o <output.svg>     coloring output, as a svg file\n");
-	fprintf(stdout, "\t-a <output.pdf>     coloring output, pdf format (mandatory)\n");
-	fprintf(stdout, "\t-g <gray level>     line color, 0-255 integer (default: 176)\n");
-	fprintf(stdout, "\t-k <gray level>     text color, 0-255 integer (default: 80)\n");
-	fprintf(stdout, "\t-x <pixel size>     size of one pixel, in millimeters (default: 2.0)\n");
-	fprintf(stdout, "\t-w <page width>     page width in millimeters (default: 210.0)\n");
-	fprintf(stdout, "\t-f <page height>    page height in millimeters (default: 297.0)\n");
-	fprintf(stdout, "\t-t <top margin>     top page margin in millimeters (all margins default to 5.0)\n");
-	fprintf(stdout, "\t-b <bottom margin>  bottom page margin\n");
-	fprintf(stdout, "\t-l <left margin>    left page margin\n");
-	fprintf(stdout, "\t-r <right margin>   right page margin\n");
-	fprintf(stdout, "\t-s                  print colored outputs, instead of numbered ones\n");
-
-	exit(EXIT_FAILURE);
+    fprintf(stderr, "%s: \"%s\"\n",
+            qPrintable(QCoreApplication::translate("main", "Mandatory option missing")),
+            str);
 }
-
 
 int main(int argc, char *argv[])
 {
-	// Main vars
-	struct Coloring coloring;
-	struct col_opt opts;
+    // QApp
+    QCoreApplication app(argc, argv);
+    QCoreApplication::setApplicationName("PixArtGen");
+    QCoreApplication::setApplicationVersion("0.0.1");
+    QLocale locale;
 
-	QGuiApplication a(argc, argv);
+    // Options
+    struct Coloring coloring;
+    struct col_opt opts;
+    QString paletteFile;
+    QString inputFile;
+    QString outputFile;
+    bool needColour = false;
 
-	// hack to force period as the decimal separator
-	setlocale(LC_NUMERIC, "C");
+    // Command line parsing
+    bool mandatoryOptionsMissing = false;
+    QCommandLineParser parser;
+    parser.setApplicationDescription(QCoreApplication::translate("main", "Pixel art and colouring generator"));
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addOptions({
+                          // color palette (mandatory)
+                          {{"p", "palette"},
+                           QCoreApplication::translate("main", "Set color palette to <palette.csv> (mandatory)"),
+                           QCoreApplication::translate("main", "palette.csv")},
+                          // Input file (mandatory)
+                          {{"i", "input"},
+                           QCoreApplication::translate("main", "Input picture (mandatory)"),
+                           QCoreApplication::translate("main", "file")},
+                          // Output file (mandatory)
+                          {{"o", "output"},
+                           QCoreApplication::translate("main", "Output file (mandatory)"),
+                           QCoreApplication::translate("main", "file")},
+                          // Output line color
+                          {{"g", "line-color"},
+                           QCoreApplication::translate("main", "Line color, 0-255 integer gray level (default: 176)"),
+                           QCoreApplication::translate("main", "integer")},
+                          // Output text color
+                          {{"k", "text-color"},
+                           QCoreApplication::translate("main", "Text color, 0-255 integer gray level (default: 80)"),
+                           QCoreApplication::translate("main", "integer")},
+                          // Pixel size (mm)
+                          {{"x", "pixel-size"},
+                           QCoreApplication::translate("main", "Pixel size, in millimeters (default: 2mm)"),
+                           QCoreApplication::translate("main", "float")},
+                          // Page width
+                          {{"w", "page-width"},
+                           QCoreApplication::translate("main", "Page width in millimeters (defaults to A4 width: 210mm)"),
+                           QCoreApplication::translate("main", "float")},
+                          // Page height
+                          {{"f", "page-height"},
+                           QCoreApplication::translate("main", "Page height in millimeters (defaults to A4 height: 297mm)"),
+                           QCoreApplication::translate("main", "float")},
+                          // Page top margin
+                          {{"t", "margin-top"},
+                           QCoreApplication::translate("main", "Page top margin  (default: 5mm)"),
+                           QCoreApplication::translate("main", "float")},
+                          // Page left margin
+                          {{"l", "margin-left"},
+                           QCoreApplication::translate("main", "Page left margin (default: 5mm)"),
+                           QCoreApplication::translate("main", "float")},
+                          // Page right margin
+                          {{"r", "margin-right"},
+                           QCoreApplication::translate("main", "Page right margin (default: 5mm)"),
+                           QCoreApplication::translate("main", "float")},
+                          // Bottom margin
+                          {{"b", "margin-bottom"},
+                           QCoreApplication::translate("main", "Page bottom margin (default: 5 mm)"),
+                           QCoreApplication::translate("main", "float")},
+                          // Coloured output
+                          {{"c", "color-output"},
+                           QCoreApplication::translate("main", "Coloured output")}
+                      });
 
-	/*
-	 * Default opt values
-	 */
-	// Once again... cpp crap...
-	// Default options (A4 paper, 5 mm margins, 2 mm pixel size)
-	opts.page.width    = 210.0;
-	opts.page.height   = 297.0;
-	opts.margin.top    = 5.0;
-	opts.margin.bottom = 5.0;
-	opts.margin.right  = 5.0;
-	opts.margin.left   = 5.0;
-	opts.px_size       = 2.0;
-	opts.lineColor     = 176; // 0xB0
-	opts.textColor     = 80;  // 0x50
+    // Parse command line
+    parser.process(app);
 
-	/*
-	 * command line parsing
-	 */
-	int opt;
-	char *palette_file = NULL;
-	char *input_file = NULL;;
-	char *output_svg = NULL;
-	char *output_pdf = NULL;
-	bool need_colors = false;
+    if (!parser.isSet("palette")) {
+        printMissingOption("palette");
+        mandatoryOptionsMissing = true;
+    }
+    if (!parser.isSet("input")) {
+        printMissingOption("input");
+        mandatoryOptionsMissing = true;
+    }
+    if (!parser.isSet("output")) {
+        printMissingOption("output");
+        mandatoryOptionsMissing = true;
+    }
 
-	while ( (opt = getopt(argc, argv, "hp:i:o:a:x:w:f:t:b:l:r:sg:k:")) != -1 ) {
-		string str;
-		double convstr;
-		int result;
-		switch (opt) {
-		case 'h':
-			help_exit(argv[0]);
-			break;
-		case 'p':
-			palette_file = optarg;
-			break;
-		case 'i':
-			input_file = optarg;
-			break;
-		case 'o':
-			output_svg = optarg;
-			break;
-		case 'a':
-			output_pdf = optarg;
-			break;
-		case 'x':
-			str = optarg;
-			convstr = stod(str);
-			if (convstr <= 0) {
-				fprintf(stderr, "Invalid pixel size (must be positive)\n");
-				exit(EXIT_FAILURE);
-			}
-			opts.px_size = convstr;
-			break;
-		case 'w':
-			str = optarg;
-			convstr = stod(str);
-			if (convstr <= 0) {
-				fprintf(stderr, "Page width must be positive\n");
-				exit(EXIT_FAILURE);
-			}
-			opts.page.width = convstr;
-			break;
-		case 'f':
-			str = optarg;
-			convstr = stod(str);
-			if (convstr <= 0) {
-				fprintf(stderr, "Page height must be positive\n");
-				exit(EXIT_FAILURE);
-			}
-			opts.page.height = convstr;
-			break;
-		case 't':
-			str = optarg;
-			convstr = stod(str);
-			if (convstr < 0) {
-				fprintf(stderr, "Top margin must be positive\n");
-				exit(EXIT_FAILURE);
-			}
-			opts.margin.top = convstr;
-			break;
-		case 'b':
-			str = optarg;
-			convstr = stod(str);
-			if (convstr < 0) {
-				fprintf(stderr, "Bottom margin must be positive\n");
-				exit(EXIT_FAILURE);
-			}
-			opts.margin.bottom = convstr;
-			break;
-		case 'l':
-			str = optarg;
-			convstr = stod(str);
-			if (convstr < 0) {
-				fprintf(stderr, "Left margin must be positive\n");
-				exit(EXIT_FAILURE);
-			}
-			opts.margin.left = convstr;
-			break;
-		case 'r':
-			str = optarg;
-			convstr = stod(str);
-			if (convstr < 0) {
-				fprintf(stderr, "Right margin must be positive\n");
-				exit(EXIT_FAILURE);
-			}
-			opts.margin.right = convstr;
-			break;
-		case 's':
-			need_colors = true;
-			break;
-		case 'g':
-			str = optarg;
-			result = stoi(str, nullptr, 0);
-			if (result < 0 || result > 255) {
-				fprintf(stderr, "Invalid line color: %d, must be in 0-255 interval\n", result);
-				exit(EXIT_FAILURE);
-			}
-			opts.lineColor = result;
-			break;
-		case 'k':
-			str = optarg;
-			result = stoi(str, nullptr, 0);
-			if (result < 0 || result > 255) {
-				fprintf(stderr, "Invalid text color: %d, must be in 0-255 interval\n", result);
-				exit(EXIT_FAILURE);
-			}
-			opts.textColor = result;
-			break;
-		default:
-			fprintf(stderr, "Unhandled option: %c\n", (char)opt);
-		}
-	}
-	// Check for missing mandatory options
-	int missing_opts = 0;
-	if (palette_file == NULL) {
-		fprintf(stderr, "Palette file is missing\n");
-		missing_opts += 1;
-	}
-	if (input_file == NULL) {
-		fprintf(stderr, "Input file is missing\n");
-		missing_opts += 1;
-	}
-	if ( (output_svg == NULL) && (output_pdf == NULL) ) {
-		fprintf(stderr, "No output file: nothing to do.\n");
-		missing_opts += 1;
-	}
-	if (missing_opts) {
-		fprintf(stderr, "Aborting\n");
-		exit(EXIT_FAILURE);
-	}
+    if (mandatoryOptionsMissing) {
+        exit(EXIT_FAILURE);
+    }
 
-	make_coloring(palette_file, input_file, opts, coloring);
-	if (output_pdf)
-		coloring2pdf(output_pdf, coloring, opts, need_colors);
-	//if (output_svg)
-		//coloring2svg(output_svg, coloring, opts, need_colors);
+    // Retrieve parameters from command line
+    paletteFile = parser.value("palette");
+    inputFile = parser.value("input");
+    outputFile = parser.value("output");
+    if (parser.isSet("line-color")) {
+        QString str = parser.value("line-color");
+        bool ok;
+        int value = locale.toInt(str, &ok);
+        if (!ok) {
+            fprintf(stderr, "%s: %s\n",
+                    qPrintable(QCoreApplication::translate("main", "Invalid line color value, can't be converted to an int")),
+                    qPrintable(str));
+            exit(EXIT_FAILURE);
+        }
+        if (value < 0 || value > 255) {
+            fprintf(stderr, "%s: %d\n",
+                    qPrintable(QCoreApplication::translate("main", "Invalid range for line color (valid range: 0-255)")),
+                    value);
+            exit(EXIT_FAILURE);
+        }
+        opts.lineColor = value;
+    } else {
+        opts.lineColor = 176;
+    }
+    if (parser.isSet("text-color")) {
+        QString str = parser.value("text-color");
+        bool ok;
+        int value = locale.toInt(str, &ok);
+        if (!ok) {
+            fprintf(stderr, "%s: %s\n",
+                    qPrintable(QCoreApplication::translate("main", "Invalid text color value")),
+                    qPrintable(str));
+            exit(EXIT_FAILURE);
+        }
+        if (value < 0 || value > 255) {
+            fprintf(stderr, "%s: %d\n",
+                    qPrintable(QCoreApplication::translate("main", "Invalid range for text color (expected: 0-255):")),
+                    value);
+            exit(EXIT_FAILURE);
+        }
+        opts.textColor = value;
+    } else {
+        opts.textColor = 80;
+    }
+    if (parser.isSet("page-width")) {
+        QString str = parser.value("page-width");
+        bool ok;
+        double value = locale.toDouble(str, &ok);
+        if (!ok) {
+            fprintf(stderr, "%s: %s\n",
+                    qPrintable(QCoreApplication::translate("main", "Invalid page width value")),
+                    qPrintable(str));
+            exit(EXIT_FAILURE);
+        }
+        if (value <= 0) {
+            fprintf(stderr, "%s: %f\n",
+                    qPrintable(QCoreApplication::translate("main", "Page width must be positive")),
+                    value);
+            exit(EXIT_FAILURE);
+        }
+        opts.page.width = value;
+    } else {
+        opts.page.width = 210;
+    }
+    if (parser.isSet("page-height")) {
+        QString str = parser.value("page-height");
+        bool ok;
+        double value = locale.toDouble(str, &ok);
+        if (!ok) {
+            fprintf(stderr, "%s: %s\n",
+                    qPrintable(QCoreApplication::translate("main", "Invalid page height value")),
+                    qPrintable(str));
+            exit(EXIT_FAILURE);
+        }
+        if (value <= 0) {
+            fprintf(stderr, "%s: %f\n",
+                    qPrintable(QCoreApplication::translate("main", "Page height must be positive")),
+                    value);
+            exit(EXIT_FAILURE);
+        }
+        opts.page.height = value;
+    } else {
+        opts.page.height = 297;
+    }
+    if (parser.isSet("margin-top")) {
+        QString str = parser.value("margin-top");
+        bool ok;
+        double value = locale.toDouble(str, &ok);
+        if (!ok) {
+            fprintf(stderr, "%s: %s\n",
+                    qPrintable(QCoreApplication::translate("main", "Invalid top margin value")),
+                    qPrintable(str));
+            exit(EXIT_FAILURE);
+        }
+        if (value <= 0) {
+            fprintf(stderr, "%s: %f\n",
+                    qPrintable(QCoreApplication::translate("main", "Top margin must be positive")),
+                    value);
+            exit(EXIT_FAILURE);
+        }
+        if (value >= opts.page.height) {
+            fprintf(stderr, "%s\n",
+                    qPrintable(QCoreApplication::translate("main", "Top margin must be smaller than page height")));
+            exit(EXIT_FAILURE);
+        }
+        opts.margin.top = value;
+    } else {
+        opts.margin.top = 5;
+    }
+    if (parser.isSet("margin-bottom")) {
+        QString str = parser.value("margin-bottom");
+        bool ok;
+        double value = locale.toDouble(str, &ok);
+        if (!ok) {
+            fprintf(stderr, "%s: %s\n",
+                    qPrintable(QCoreApplication::translate("main", "Invalid bottom margin value")),
+                    qPrintable(str));
+            exit(EXIT_FAILURE);
+        }
+        if (value <= 0) {
+            fprintf(stderr, "%s\n",
+                    qPrintable(QCoreApplication::translate("main", "Bottom margin must be positive")));
+            exit(EXIT_FAILURE);
+        }
+        if (value >= opts.page.height - opts.margin.top) {
+            fprintf(stderr, "%s\n",
+                    qPrintable(QCoreApplication::translate("main", "Top and bottom margins are bigger than page height")));
+            exit(EXIT_FAILURE);
+        }
+        opts.margin.bottom = value;
+    } else {
+        opts.margin.bottom = 5;
+    }
+    if (parser.isSet("margin-left")) {
+        QString str = parser.value("margin-left");
+        bool ok;
+        double value = locale.toDouble(str, &ok);
+        if (!ok) {
+            fprintf(stderr, "%s: %s\n",
+                    qPrintable(QCoreApplication::translate("main", "Invalid left margin value")),
+                    qPrintable(str));
+            exit(EXIT_FAILURE);
+        }
+        if (value <= 0) {
+            fprintf(stderr, "%s\n",
+                    qPrintable(QCoreApplication::translate("main", "Left margin must be positive")));
+            exit(EXIT_FAILURE);
+        }
+        if (value >= opts.page.width) {
+            fprintf(stderr, "%s\n",
+                    qPrintable(QCoreApplication::translate("main", "Left margin must be smaller than the page width")));
+            exit(EXIT_FAILURE);
+        }
+        opts.margin.left = value;
+    } else {
+        opts.margin.left = 5;
+    }
+    if (parser.isSet("margin-right")) {
+        QString str = parser.value("margin-right");
+        bool ok;
+        double value = locale.toDouble(str, &ok);
+        if (!ok) {
+            fprintf(stderr, "%s: %s\n",
+                    qPrintable(QCoreApplication::translate("main", "Invalid left margin value")),
+                    qPrintable(str));
+            exit(EXIT_FAILURE);
+        }
+        if (value <= 0) {
+            fprintf(stderr, "%s\n",
+                    qPrintable(QCoreApplication::translate("main", "Bottom margin must be positive")));
+            exit(EXIT_FAILURE);
+        }
+        if (value >= opts.page.width - opts.margin.left) {
+            fprintf(stderr, "%s\n",
+                    qPrintable(QCoreApplication::translate("main", "Left and right margins exceed page width")));
+            exit(EXIT_FAILURE);
+        }
+        opts.margin.right = value;
+    } else {
+        opts.margin.right = 5;
+    }
+    if (parser.isSet("pixel-size")) {
+        QString str = parser.value("pixel-size");
+        bool ok;
+        double value = locale.toDouble(str, &ok);
+        if (!ok) {
+            fprintf(stderr, "%s: %s\n",
+                    qPrintable(QCoreApplication::translate("main", "Invalid pixel size")),
+                    qPrintable(str));
+            exit(EXIT_FAILURE);
+        }
+        if (value <= 0) {
+            fprintf(stderr, "%s\n",
+                    qPrintable(QCoreApplication::translate("main", "Pixel size must be positive")));
+            exit(EXIT_FAILURE);
+        }
+        if (value >= opts.page.height - opts.margin.top - opts.margin.bottom
+                || value >= opts.page.width - opts.margin.left - opts.margin.right) {
+            fprintf(stderr, "%s\n",
+                    qPrintable(QCoreApplication::translate("main", "Pixel size exceeds available width and/or height")));
+            exit(EXIT_FAILURE);
+        }
+        opts.px_size = value;
+    } else {
+        opts.px_size = 2;
+    }
+    if (parser.isSet("color-output")) {
+        needColour = true;
+    } else {
+        needColour = false;
+    }
+
+    make_coloring(paletteFile.toLocal8Bit().constData(),
+                  inputFile.toLocal8Bit().constData(),
+                  opts,
+                  coloring);
+    coloring2pdf(outputFile.toLocal8Bit().constData(),
+                 coloring,
+                 opts,
+                 needColour);
 
 	QTimer::singleShot(0, QCoreApplication::instance(), SLOT(quit()));
 
-	return a.exec();
+    return app.exec();
 }
-
 
